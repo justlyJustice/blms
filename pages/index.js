39e46d35186c6f6187ddbe7654aa0b182416e385
Justice Johnson <justlyjohn198@gmail.com>
2023-02-16 11:36:01 +0100

    Staged changes

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <Head
         title="BLMS"
         description="BLMS is a learning management software application or web-based technology used to plan, implement and assess specific learning process. It is used for eLearning practices and, in its most common form, consists of two elements: a server that performs the base functionality and a user interface that is operated by instructors, students and administrators."
+        image="/assets/logo.png"
       />
 
       <HeroSection />
